4461be26b9fe2c6d07267c677442729c6ef30eb4
Lance Martin <ldmartin7777@gmail.com>
2016-08-03 11:37:53 -0500

    can sequelize.findAll()

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// app.use(bodyParser.urlencoded({
+// 	extended: false
+// }));
+
+router.get('/', function(req,res) {
+	res.redirect('/burgers');
+});
+
+router.get('/burgers', function(req,res) {
+	models.burger.findAll().then(function(data){
+		var hbsObject = {burger : data};
+		console.log(data);
+		res.render('index', hbsObject);
+	});
+	// burger.selectAll(function(data){
+	// 	var hbsObject = {burger : data};
+	// 	console.log(hbsObject);
+	// 	res.render('index', hbsObject);
+	// });
+});
+
+// router.post('/burgers/create', function(req,res) {
+//   console.log('post requested');
+//   console.log(req.body);
+// 	burger.insertOne(req.body.name, false, function(data){
+// 		res.redirect('/burgers');
+// 	});
+// });
+//
+// router.put('/burgers/update/:id', function(req,res) {
+//
+//  	burger.updateOne(req.params.id, req.body.eat, function(data){
+//  		res.redirect('/burgers');
+//    	});
+// });
+
+module.exports = router;
