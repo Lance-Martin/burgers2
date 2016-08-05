cc783033951908c51797defe9cd0c7861aa0ca20
Lance Martin <ldmartin7777@gmail.com>
2016-08-05 14:55:44 -0500

    ready to deploy without jawsDB connection

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,9 +4,6 @@ var models = require('../models');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-// app.use(bodyParser.urlencoded({
-// 	extended: false
-// }));
 
 router.get('/', function(req,res) {
 	res.redirect('/burgers');
@@ -15,14 +12,11 @@ router.get('/', function(req,res) {
 router.get('/burgers', function(req,res) {
 	models.burger.findAll().then(function(data){
 		var hbsObject = {burger : data};
-		console.log(data);
 		res.render('index', hbsObject);
 	});
 });
 
 router.post('/burgers/create', function(req,res) {
-  console.log('post requested');
-  console.log(req.body);
 	models.burger.create({
         name: req.body.name,
         devoured: false,
@@ -41,10 +35,6 @@ router.put('/burgers/update/:id', function(req,res) {
 		});
   }
 	});
-
- // 	burger.updateOne(req.params.id, req.body.eat, function(data){
- // 		res.redirect('/burgers');
-  //  	});
 });
 
 module.exports = router;
--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Blue cheese mushroom', false, now());
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bacon and bbq sauce', false, now());
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('The classic', false, now());
