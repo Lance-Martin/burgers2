702ea59021c6c39da84f1e04777e30b1f6487f4c
Lance Martin <ldmartin7777@gmail.com>
2016-08-05 20:13:32 -0500

    added seeder

---
+++ seeders/20160806010922-unnamed-seeder.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('burgers', [{
+      name: 'The classic',
+      devoured: false
+    },
+    {
+      name: 'The diablo',
+      devoured: false
+    },
+    {
+      name: 'The jaw dropper',
+      devoured: false
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('burgers', null, {});
+  }
+};
